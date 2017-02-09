144c10d6ccf3524dde715077f1ae66633198b4ec
Yanis Wang <yanis.wangy@alibaba-inc.com>
2017-02-09 15:36:36 +0800

    Add chai test

--- test/chai.spec.js
+++ test/chai.spec.js
@@ -6,18 +6,35 @@ chai.should();
 chai.use(PromiseClass.chaiSupportChainPromise);
 
 var App = PromiseClass.create({
-    asyncMethod(n, done){
+    asyncMethod1(n, done){
         setTimeout(function(){
             done(null, n);
         }, n);
     },
+    asyncMethod2(done){
+        setTimeout(function(){
+        	done(new Error('ttt'))
+        }, 100)
+    },
+    asyncMethod3(){
+        asdfa();
+    }
 });
 
 var app = new App();
 
 describe('chai test : ', function(){
-    it('async method test', function(){
-        return app.asyncMethod(100).should.equal(100)
-        .asyncMethod(88).should.equal(88);
+    it('async method test1', function(){
+        return app.asyncMethod1(100).should.equal(100)
+        .asyncMethod1(88).should.equal(88);
+    });
+
+    it('async method test2', function(){
+		return app.asyncMethod1(10).should.not.be.a('error')
+		.asyncMethod2().should.be.a('error');
+    });
+
+    it('async method test3', function(){
+        return app.asyncMethod3().should.be.a('error');
     });
 });
